549af02764247755972e69cb22ddcdf7e4a9be88
crackslinger <fbueller2234@protonmail.com>
2023-02-02 04:19:15 +0100

    crawl single page with error handling

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log("Hello")
+const {crawlPage} = require('./crawl.js');
+
+
+function main() {
+    if (process.argv.length < 3) {
+        console.log("no web site provided");
+        process.exit(1);
+    }
+    if (process.argv.length > 3) {
+        console.log("too many command line args");
+        process.exit(1);
+    }
+
+    const baseURL = process.argv[2];
+
+    console.log(`starting crawl of ${baseURL}`);
+    crawlPage(baseURL);
+
+
+}
+
+main()
