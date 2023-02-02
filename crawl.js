19923693e436e3cb702e18df1fa69d35c9001ce4
crackslinger <fbueller2234@protonmail.com>
2023-02-02 18:26:08 +0100

    crawl page recursively

--- crawl.js
+++ crawl.js
@@ -1,26 +1,48 @@
 const { JSDOM } = require('jsdom');
 
-async function crawlPage(currentURL) {
-    console.log(`actively crawling: ${currentURL}`);
-    
-    try {
-        const resp = await fetch(currentURL);
+async function crawlPage(baseURL, currentURL, pages) {
+    const baseURLObj = new URL(baseURL);
+    const currentURLObj = new URL(currentURL);
+    if (baseURLObj.hostname !== currentURLObj.hostname) {
+        return pages;
+    }
 
+    const normalizedCurrentURL = normalizeURL(currentURL)
+    if (pages[normalizedCurrentURL] > 0) {
+        pages[normalizedCurrentURL]++;
+        return pages;
+    }
+
+    pages[normalizedCurrentURL] = 1;
+
+    console.log(`actively crawling: ${currentURL}`);
+    let htmlBody = '';
+    
+    try { 
+        const resp = await fetch(currentURL);
         if (resp.status > 399) {
             console.log(`error in fetch with status code ${resp.status} on page: ${currentURL}`);
-            return
+            return pages;
         }
 
         const contentType = resp.headers.get("content-type");
         if (!contentType.includes("text/html")) {
-            console.log(`non html response, content type ${contentType} on page: ${currentPage}`);
-            return
+            console.log(`non html response, content type ${contentType} on page: ${currentURL}`);
+            return pages;
         }
 
-        console.log(await resp.text());
+        htmlBody = await resp.text();
+
     } catch (err) {
         console.log(`error in fetch: ${err.message}, on page ${currentURL}`);
     }
+
+    const nextURLs = getURLsFromHTML(htmlBody, baseURL);
+    for (const nextURL of nextURLs) {
+        pages = await crawlPage(baseURL, nextURL, pages);
+    }
+
+    return pages;
 }
 
 
